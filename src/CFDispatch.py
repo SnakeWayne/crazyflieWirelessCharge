6bbb98fd9194262fe4ab1796a192c23f5567c8ce
ZZW <947518671@qq.com>
2019-04-07 17:28:42 +0800

    zzw CFDispatch

--- src/CFDispatch.py
+++ src/CFDispatch.py
@@ -25,36 +25,34 @@ class CFDispatch():
             elif status_list[i].current_posture == FlyPosture.charging:
                 list_charging.append(status_list[i])
 
-        _min_battery = 100
-        _max_battery = 0
-        _flying_patch = 0  # 飞行中需要交换的无人机
-        _charging_patch = 1  # 充电中需要交换的无人机
-        _safe_battery = 20  # 安全电量
+        min_battery = 100
+        max_battery = 0
+        flying_patch = 0  # 飞行中需要交换的无人机
+        charging_patch = 1  # 充电中需要交换的无人机
+        safe_battery = 20  # 安全电量
 
         # 找出飞行中电量最低的无人机
         for i in list_flying:
-            if list_flying[i].current_battery < _min_battery:
-                _flying_patch = i
-                _min_battery = list_flying[i].current_battery
+            if list_flying[i].current_battery < min_battery:
+                flying_patch = i
+                min_battery = list_flying[i].current_battery
 
         # 找出充电中电量最高的无人机
         for i in list_charging:
-            if list_charging[i].current_battery > _max_battery:
-                _charging_patch = i
-                _max_battery = list_charging[i].current_battery
+            if list_charging[i].current_battery > max_battery:
+                charging_patch = i
+                max_battery = list_charging[i].current_battery
 
         # 虽说初始状态不一定，但这样设置能满足初始电量一样或电量差比较多的情况，不仅有一个底线，还有电量都比较多情况下的梯度变化
-        _patch_min_battery = 80 - n * 20  # 需要调度的最低变量
+        patch_min_battery = 80 - _n * 20  # 需要调度的最低变量
         _n = _n + 1
-        if _patch_min_battery < _safe_battery:
-            _patch_min_battery = _safe_battery
+        if patch_min_battery < safe_battery:
+            patch_min_battery = safe_battery
 
         # 判断一下是否需要调度
-        if _min_battery < _patch_min_battery:
-            return list_flying[_flying_patch].url, list_charging[_charging_patch].url
-        elif len(list_charging) == 0:
-            return "abort", "abort"
-        elif _max_battery < _safe_battery:
+        if min_battery < patch_min_battery:
+            return list_flying[flying_patch].url, list_charging[charging_patch].url
+        elif max_battery < safe_battery:
             return "abort", "abort"
         else:
             return "radio", "radio"
