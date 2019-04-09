2b08a89d7699f9a2902d6f6a5fd54cd8345d58bf
a779806044 <38976245+a779806044@users.noreply.github.com>
2019-04-09 18:43:37 +0800

    Update Fly_attitude.py

--- Fly_attitude.py
+++ Fly_attitude.py
@@ -6,10 +6,10 @@ from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.crazyflie.syncLogger import SyncLogger
 
 class FlyPosture(Enum):
-    flying=1
-    hover=2
-    changing=3
-    charging=4
+    flying=1 #正在飞行中
+    hover=2  #等待队列调整
+    changing=3 #被选中无人机正在进行调度
+    charging=4 #正在充电
  
  
 class CFSequence:
@@ -29,7 +29,7 @@ class CFSequence:
      
 class CFStatus:
      url='radio://0/40/2M/E7E7E7E7E7'
-     current_position=[x, y, z]
+     current_position=[x, y, z]  #不再单独存储无线充电座位置，如果发现posture是充电中，则就认为此position为充电座position
      current_battery=电量
      current_posture=FlyPosture   #(刻画当前状态)
      
