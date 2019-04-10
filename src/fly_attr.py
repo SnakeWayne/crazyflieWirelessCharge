7f848da1aead0c12db57a55b9034ddeb3c196314
SnakeWayne <526588851@qq.com>
2019-04-10 18:18:36 +0800

    fly attribute add

---
+++ src/fly_attr.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+
+
+class FlyPosture(Enum):
+
+    flying = 1  # 正在飞行中
+
+    hover = 2   # 等待队列调整
+
+    changing = 3  # 被选中无人机正在进行调度
+
+    charging = 4  # 正在充电
+
+
+class CFSequence:
+
+    def __init__(self, sequence, uri, current_sequence_index=0):
+        """
+        初始化参数，不允许中途修改
+        :param sequence: 飞行任务队列四元组，飞行具体位置xyz，飞行悬停时间s
+        :param uri: 无人机uri
+        :param current_sequence_index: 当前飞行任务序列下标
+        """
+        self._sequence = sequence
+        self._uri = uri
+        self._current_sequence_index = current_sequence_index
+
+    def __getattr__(self, item):
+        if item == 'sequence':
+            return self._sequence
+        elif item == 'uri':
+            return self._uri
+        elif item == 'current_sequence_index':
+            return self._current_sequence_index
+        elif item == 'current_sequence':
+            return self._sequence[self._current_sequence_index]
+        elif item == 'next':  # 仿写java iterable的风格
+            self._current_sequence_index += 1
+            if self._current_sequence_index <= len(self._sequence):
+                return self._sequence[self.current_sequence_index - 1]
+            else:
+                return None
+        else:
+            raise AttributeError('没有这个属性')
+
+
+class CFStatus:
+    def __init__(self, uri, current_position=[0, 0, 0], current_battery=100,current_posture=FlyPosture.flying):
+        """
+        初始化时瞎赋值，只要求uri必须提供就行了
+        :param uri: 无人机地址
+        :param current_position: xyz三元组
+        :param current_battery: 100为满电，0为无电
+        :param current_posture: FlyPosture所定义的几种姿态
+        """
+        self._uri = uri
+        self._current_position = current_position
+        self._current_battery = current_battery
+        self._current_posture = current_posture
+
+    def __getattr__(self, item):
+        if item == 'uri':
+            return self._uri
+        elif item == 'current_position':
+            return self._current_position
+        elif item == 'current_battery':
+            return self._current_battery
+        elif item == 'current_posture':
+            return self._current_posture
+        else:
+            raise AttributeError('没有这个属性')
+
+    def __setattr__(self, key, value):
+        if key == 'uri':
+            super().__setattr__('_uri', value)
+        elif key == 'current_position':
+            super().__setattr__('_current_position', value)
+        elif key == 'current_battery':
+            super().__setattr__('_current_battery', value)
+        elif key == 'current_posture':
+            super().__setattr__('_current_posture', value)
+        else:
+            super().__setattr__(key, value)
+
+
+
